4969debdca2dcf8bb4f1e6d0c72177720f5fdc10
JJ Ying <yingjunjiu@gmail.com>
2017-10-23 16:06:47 +0800

    移动了资源文件位置

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var gulp = require('gulp'),
     plugins = gulpLoadPlugins();
  
 var cdnUrl = [
-	[ 'assets/', 'http://anyway-web.b0.upaiyun.com/iconmoon/' ]
+	[ 'assets/jquery-3.1.1.min.js', 'http://anyway-web.b0.upaiyun.com/js/jquery-3.1.1.min.js' ],
+	[ 'assets/in-view.min.js', 'http://anyway-web.b0.upaiyun.com/js/in-view.min.js' ],
+	[ 'assets/', 'http://anyway-web.b0.upaiyun.com/iconmoon-2017/' ]
 ];
  
 gulp.task('watch', function() {
@@ -15,9 +17,14 @@ gulp.task('default', function() {
 
 	gulp.src('*.html')
 		.pipe(plugins.batchReplace(cdnUrl))
+		.pipe(plugins.htmlMinifier({
+			collapseWhitespace: true,
+			removeComments: true,
+			minifyJS: true
+		}))
 		.pipe(gulp.dest('builds'));
 		
-	gulp.src('*.css')
+	gulp.src('assets/*.css')
 		.pipe(plugins.cleanCss({compatibility: 'ie8'}))
 		.pipe(gulp.dest('builds'));     
 });

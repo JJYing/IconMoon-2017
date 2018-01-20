7c44ccd64fed405e45c5b41aea395e776de571bf
JJ Ying <yingjunjiu@gmail.com>
2018-01-20 22:21:12 +0800

    更换了 CDN 地址

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
 var gulp = require('gulp'),
     gulpLoadPlugins = require('gulp-load-plugins'),
     plugins = gulpLoadPlugins();
- 
+
 var cdnUrl = [
-	[ 'assets/jquery-3.1.1.min.js', 'http://anyway-web.b0.upaiyun.com/js/jquery-3.1.1.min.js' ],
-	[ 'assets/in-view.min.js', 'http://anyway-web.b0.upaiyun.com/js/in-view.min.js' ],
-	[ 'assets/', 'http://anyway-web.b0.upaiyun.com/iconmoon-2017/' ]
+	[ 'assets/jquery-3.1.1.min.js', 'http://anw.red/js/jquery-3.1.1.min.js' ],
+	[ 'assets/in-view.min.js', 'http://anw.red/js/in-view.min.js' ],
+	[ 'assets/', 'http://anw.red/iconmoon-2017/' ]
 ];
- 
+
 gulp.task('watch', function() {
 	gulp.watch(['*','*/*'], ['default']);
  });
  
- 
+
 gulp.task('default', function() {
 
 	gulp.src('*.html')
@@ -23,9 +23,8 @@ gulp.task('default', function() {
 			minifyJS: true
 		}))
 		.pipe(gulp.dest('builds'));
-		
+
 	gulp.src('assets/*.css')
 		.pipe(plugins.cleanCss({compatibility: 'ie8'}))
-		.pipe(gulp.dest('builds'));     
+		.pipe(gulp.dest('builds'));
 });
-
